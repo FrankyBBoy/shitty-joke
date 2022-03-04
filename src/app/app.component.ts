c83d4706a1e8664180a07ee2f970479b1c72ad95
FrankyBBoy <francis1592@gmai.com>
2022-03-03 20:00:36 -0500

    WIP design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faThumbsUp } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shitty-joke';
+  faThumbsUp = faThumbsUp;
 }
